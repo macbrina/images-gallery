756d071fc69190056e959824de4538c2c1efd209
Mac Arthur <preciousmbaekwe@gmail.com>
2023-06-08 04:39:58 +0100

    Controlled search component

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,12 +1,18 @@
 import React from 'react';
-import { Navbar } from 'react-bootstrap';
+import { Navbar, Container } from 'react-bootstrap';
+
+const navbarStyle = {
+     backgroundColor: 'lightblue'
+};
 
 const Header = ({ title }) => {
     return (
-      <Navbar bg="light" variant="light">
+      <Navbar style={navbarStyle} variant="light">
+        <Container>
           <Navbar.Brand href="/">{title}</Navbar.Brand>
+        </Container>
       </Navbar>
-    )
+    );
 };
 
 export default Header;
